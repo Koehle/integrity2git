5fcf2b7c3313d9b37bf2976c171bc4b51c26ed1a
koehmark <markus.koehlinger@keba.com>
2022-01-28 12:17:29 +0100

    Modifications for Windows 10 / Python 3.10 / MKS Integrity 2007: - Replaced print() function because of trouble with CRLF line endings.   We use "sys.stdout.buffer.write()" instead. - Added replacement for German umlauts (see link to source of this code). - Some changes for the processing of the MKS project history:   Export not only the MKS checkpoint label, but also the checkpoint description.   Consideration of German umlauts in the version description and author name. - The python script can be located outside of the new GIT directory now.   As argument the GIT directory can be passed (e.g. via batch file).

--- mks_checkpoints_to_git.py
+++ mks_checkpoints_to_git.py
@@ -9,21 +9,48 @@ import sys
 import re
 import platform
 from datetime import datetime
+
 # this is so windows doesn't output CR (carriage return) at the end of each line and just does LF (line feed)
 if platform.system() == 'Windows':
     import msvcrt
-    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)    # did not work im my environment!?!
 
-def export_data(string):
-    print 'data %d\n%s' % (len(string), string)
+# because the binary output of "print()" function did not work, it was replaced by "sys.stdout.buffer.write()"
+# binary output this way to prevent Windows from adding CR LF (carriage return line feed) at the end of each line!
+# for git fast-import only LF (line feed) is accepted!
+
+# Source for the following code snippet / python script:
+# https://gist.github.com/johnberroa/cd49976220933a2c881e89b69699f2f7
+# Removes umlauts from strings and replaces them with the letter+e convention
+# :param string: string to remove umlauts from
+# :return: unumlauted string
+def remove_umlaut(string):
+    u = 'ü'.encode()
+    U = 'Ü'.encode()
+    a = 'ä'.encode()
+    A = 'Ä'.encode()
+    o = 'ö'.encode()
+    O = 'Ö'.encode()
+    ss = 'ß'.encode()
+    string = string.encode()
+    string = string.replace(u, b'ue')
+    string = string.replace(U, b'Ue')
+    string = string.replace(a, b'ae')
+    string = string.replace(A, b'Ae')
+    string = string.replace(o, b'oe')
+    string = string.replace(O, b'Oe')
+    string = string.replace(ss, b'ss')
+    string = string.decode('utf-8')
+    return string
 
 def inline_data(filename, code = 'M', mode = '644'):
     content = open(filename, 'rb').read()
     if platform.system() == 'Windows':
         #this is a hack'ish way to get windows path names to work git (is there a better way to do this?)
         filename = filename.replace('\\','/')
-    print "%s %s inline %s" % (code, mode, filename)
-    export_data(content)
+    sys.stdout.buffer.write(bytes("%s %s inline %s\n" % (code, mode, filename), 'utf-8'))
+    sys.stdout.buffer.write(bytes('data %d\n' % (os.path.getsize(filename)), 'utf-8'))
+    sys.stdout.buffer.write(content) # binary output!
 
 def convert_revision_to_mark(revision):
     if not revision in marks:
@@ -35,7 +62,7 @@ def retrieve_revisions(devpath=0):
         pipe = Popen('si viewprojecthistory --rfilter=devpath:"%s" --project="%s"' % (devpath, sys.argv[1]), shell=True, bufsize=1024, stdout=PIPE)
     else:
         pipe = Popen('si viewprojecthistory --rfilter=devpath::current --project="%s"' % sys.argv[1], shell=True, bufsize=1024, stdout=PIPE)
-    versions = pipe.stdout.read().split('\n')
+    versions = pipe.stdout.read().decode('cp850').split('\n') # decode('cp850') necessary because of german umlauts in MKS history
     versions = versions[1:]
     version_re = re.compile('[0-9]([\.0-9])+')
     revisions = []
@@ -45,17 +72,31 @@ def retrieve_revisions(devpath=0):
             version_cols = version.split('\t')
             revision = {}
             revision["number"] = version_cols[0]
-            revision["author"] = version_cols[1]
-            revision["seconds"] = int(time.mktime(datetime.strptime(version_cols[2], "%b %d, %Y %I:%M:%S %p").timetuple()))
-            revision["description"] = version_cols[5]
+            revision["author"] = remove_umlaut(version_cols[1]) # because git fast-input expects 'utf-8' for "author"
+            revision["seconds"] = int(time.mktime(datetime.strptime(version_cols[2], "%d.%m.%Y %H:%M:%S").timetuple()))
+            # version_cols[5] == MKS Checkpoint Label / version_cols[6] == MKS Checkpoint description
+            if((version_cols[5] == "") and (version_cols[6] == "")):   # as well MKS Label as description may be empty!
+                revision["description"] = "-"                          # replace empty MKS checkpoint information with "-"
+            elif((version_cols[5] != "") and (version_cols[6] == "")): # MKS Label but no Checkpoint description
+                revision["description"] = version_cols[5]
+            elif((version_cols[5] == "") and (version_cols[6] != "")): # No MKS Label but Checkpoint description
+                revision["description"] = "-" + '\n' + version_cols[6]
+            else: # MKS Label and Checkpoint description available
+                revision["description"] = version_cols[5] + '\n' + version_cols[6]
             revisions.append(revision)
+        else:
+            # No version match could be additional description lines of a previous revision,
+            # e.g. if there are control characters like '\n' inside the checkpoint description!
+            # We ignore empty "version entries" and check if "revisions list" is not empty:
+            if((version != "") and (len(revisions)>=1)):
+                revisions[len(revisions)-1]['description'] += '\n' + version # Add string to previous revision description!
     revisions.reverse() # Old to new
     re.purge()
     return revisions
 
 def retrieve_devpaths():
     pipe = Popen('si projectinfo --devpaths --noacl --noattributes --noshowCheckpointDescription --noassociatedIssues --project="%s"' % sys.argv[1], shell=True, bufsize=1024, stdout=PIPE)
-    devpaths = pipe.stdout.read()
+    devpaths = (pipe.stdout.read()).decode('utf-8')
     devpaths = devpaths [1:]
     devpaths_re = re.compile('    (.+) \(([0-9][\.0-9]+)\)\n')
     devpath_col = devpaths_re.findall(devpaths)
@@ -65,7 +106,7 @@ def retrieve_devpaths():
 
 def export_to_git(revisions,devpath=0,ancestor=0):
     abs_sandbox_path = os.getcwd()
-	integrity_file = os.path.basename(sys.argv[1])
+    integrity_file = os.path.basename(sys.argv[1])
     if not devpath: #this is assuming that devpath will always be executed after the mainline import is finished
         move_to_next_revision = 0
     else:
@@ -74,20 +115,24 @@ def export_to_git(revisions,devpath=0,ancestor=0):
         #revision_col = revision["number"].split('\.')
         mark = convert_revision_to_mark(revision["number"])
         if move_to_next_revision:
-            os.system('si retargetsandbox --project="%s" --projectRevision=%s %s/%s' % (sys.argv[1], revision["number"], abs_sandbox_path), integrity_file)
+            os.system('si retargetsandbox --project="%s" --projectRevision=%s %s/%s' % (sys.argv[1], revision["number"], abs_sandbox_path, integrity_file))
             os.system('si resync --yes --recurse ')
         move_to_next_revision = 1
         if devpath:
-            print 'commit refs/heads/devpath/%s' % devpath
+            sys.stdout.buffer.write(b'commit refs/heads/devpath/%s\n' % devpath)
         else:
-            print 'commit refs/heads/master'
-        print 'mark :%d' % mark
-        print 'committer %s <> %d +0100' % (revision["author"], revision["seconds"]) #Germany UTC time zone
-        export_data(revision["description"])
+            sys.stdout.buffer.write(b'commit refs/heads/master\n') # binary output!
+        sys.stdout.buffer.write(bytes(('mark :%d\n' % mark), 'utf-8'))
+        # According to git fast-import documentation author (name) is typically UTF-8 encoded.
+        # https://www.git-scm.com/docs/git-fast-import
+        sys.stdout.buffer.write(bytes(('committer %s <> %d +0100\n' % (revision["author"], revision["seconds"])), 'utf-8')) #Germany UTC time zone
+        # The optional encoding command indicates the encoding of the commit message.
+        sys.stdout.buffer.write(bytes(('encoding iso-8859-15\n'), 'utf-8')) #encoding for the following description ('iso-8859-15')
+        sys.stdout.buffer.write(bytes(('data %d\n%s\n' % (len(revision["description"]), revision["description"])), 'iso-8859-15'))
         if ancestor:
-            print 'from :%d' % convert_revision_to_mark(ancestor) #we're starting a development path so we need to start from it was originally branched from
+            sys.stdout.buffer.write(b'from :%d' % convert_revision_to_mark(ancestor)) #we're starting a development path so we need to start from it was originally branched from
             ancestor = 0 #set to zero so it doesn't loop back in to here
-        print 'deleteall'
+        sys.stdout.buffer.write(b'deleteall\n')
         tree = os.walk('.')
         for dir in tree:
             for filename in dir[2]:
@@ -106,6 +151,9 @@ def export_to_git(revisions,devpath=0,ancestor=0):
 marks = []
 devpaths = retrieve_devpaths()
 revisions = retrieve_revisions()
+#Change directory to GIT directory (if argument is available)
+if (len(sys.argv) > 2):
+    os.chdir('%s' % (sys.argv[2]))
 #Create a build sandbox of the first revision
 os.system('si createsandbox --populate --recurse --project="%s" --projectRevision=%s tmp' % (sys.argv[1], revisions[0]["number"]))
 os.chdir('tmp')
@@ -113,7 +161,7 @@ export_to_git(revisions) #export master branch first!!
 for devpath in devpaths:
     devpath_revisions = retrieve_revisions(devpath[0])
     export_to_git(devpath_revisions,devpath[0].replace(' ','_'),devpath[1]) #branch names can not have spaces in git so replace with underscores
-# Drop the sandbox
-shortname=sys.argv[1].replace('"', '').split('/')[-1]
-os.chdir("..")
-os.system("si dropsandbox --yes -f --delete=all tmp/%s" % (shortname))
+#Drop the sandbox
+integrity_file = os.path.basename(sys.argv[1])
+os.chdir("..") #leave 'tmp' and return to GIT directory
+os.system("si dropsandbox --yes -f --delete=all tmp/%s" % (integrity_file))
