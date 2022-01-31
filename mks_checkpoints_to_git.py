9f53b4e30c9c7fa7d238275536bc7b1e9fdb3819
koehmark <markus.koehlinger@keba.com>
2022-01-31 20:06:15 +0100

    Commissioning of the script with development paths: - The way we represent a MKS checkpoint in GIT has been changed.   We use the MKS Checkpoint revision number and Label (if present) as GIT tag.   Within the GIT commit description, we also add the MKS checkpoint description. - Sorting of development paths does not work in my case. (Code line disabled). - A 'si resync' command requires the sandbox location in case of a development path.   According to the MKS error message, to avoid "ambiguity". - Fixed MKS commands in case of development paths (sys.stdout.buffer.write(bytes())) - Exclude *.pj files only, but keep *.pjt files, so check for *.pj files was modified. - Added code to add a lightweight tag.

--- mks_checkpoints_to_git.py
+++ mks_checkpoints_to_git.py
@@ -13,7 +13,7 @@ from datetime import datetime
 # this is so windows doesn't output CR (carriage return) at the end of each line and just does LF (line feed)
 if platform.system() == 'Windows':
     import msvcrt
-    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)    # did not work im my environment!?!
+    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)    # did not work in my environment!?!
 
 # because the binary output of "print()" function did not work, it was replaced by "sys.stdout.buffer.write()"
 # binary output this way to prevent Windows from adding CR LF (carriage return line feed) at the end of each line!
@@ -74,22 +74,20 @@ def retrieve_revisions(devpath=0):
             revision["number"] = version_cols[0]
             revision["author"] = remove_umlaut(version_cols[1]) # because git fast-input expects 'utf-8' for "author"
             revision["seconds"] = int(time.mktime(datetime.strptime(version_cols[2], "%d.%m.%Y %H:%M:%S").timetuple()))
-            # version_cols[5] == MKS Checkpoint Label / version_cols[6] == MKS Checkpoint description
-            if((version_cols[5] == "") and (version_cols[6] == "")):   # as well MKS Label as description may be empty!
-                revision["description"] = "-"                          # replace empty MKS checkpoint information with "-"
-            elif((version_cols[5] != "") and (version_cols[6] == "")): # MKS Label but no Checkpoint description
-                revision["description"] = version_cols[5]
-            elif((version_cols[5] == "") and (version_cols[6] != "")): # No MKS Label but Checkpoint description
-                revision["description"] = "-" + '\n' + version_cols[6]
-            else: # MKS Label and Checkpoint description available
-                revision["description"] = version_cols[5] + '\n' + version_cols[6]
+            # version_cols[5] == MKS Checkpoint Label (may be empty)
+            if(version_cols[5] != ""):
+                revision["label"] = version_cols[5]
+            else: # in case of an empty MKS Label
+                revision["label"] = "-"
+            # version_cols[6] == MKS Checkpoint description
+            revision["description"]  = ('%s\n\n' % revision["label"]) + ('MKS Checkpoint Revision: %s\n' % revision["number"]) + ('MKS Checkpoint Description:\n\n%s\n' % version_cols[6])
             revisions.append(revision)
         else:
-            # No version match could be additional description lines of a previous revision,
+            # 'No version match' could be additional description lines of a previous revision,
             # e.g. if there are control characters like '\n' inside the checkpoint description!
             # We ignore empty "version entries" and check if "revisions list" is not empty:
             if((version != "") and (len(revisions)>=1)):
-                revisions[len(revisions)-1]['description'] += '\n' + version # Add string to previous revision description!
+                revisions[len(revisions)-1]["description"] += '\n' + version # Add string to previous revision description!
     revisions.reverse() # Old to new
     re.purge()
     return revisions
@@ -101,7 +99,7 @@ def retrieve_devpaths():
     devpaths_re = re.compile('    (.+) \(([0-9][\.0-9]+)\)\n')
     devpath_col = devpaths_re.findall(devpaths)
     re.purge()
-    devpath_col.sort(key=lambda x: map(int, x[1].split('.'))) #order development paths by version
+    #devpath_col.sort(key=lambda x: map(int, x[1].split('.'))) #order development paths by version -> does not work in my case -> !!! ToDo !!!
     return devpath_col
 
 def export_to_git(revisions,devpath=0,ancestor=0):
@@ -116,10 +114,10 @@ def export_to_git(revisions,devpath=0,ancestor=0):
         mark = convert_revision_to_mark(revision["number"])
         if move_to_next_revision:
             os.system('si retargetsandbox --project="%s" --projectRevision=%s %s/%s' % (sys.argv[1], revision["number"], abs_sandbox_path, integrity_file))
-            os.system('si resync --yes --recurse ')
+            os.system('si resync --yes --recurse --sandbox="%s/%s"' % (abs_sandbox_path, integrity_file)) # sandbox location is required in case of devpath
         move_to_next_revision = 1
         if devpath:
-            sys.stdout.buffer.write(b'commit refs/heads/devpath/%s\n' % devpath)
+            sys.stdout.buffer.write(bytes(('commit refs/heads/devpath/%s\n' % devpath), 'utf-8'))
         else:
             sys.stdout.buffer.write(b'commit refs/heads/master\n') # binary output!
         sys.stdout.buffer.write(bytes(('mark :%d\n' % mark), 'utf-8'))
@@ -130,7 +128,7 @@ def export_to_git(revisions,devpath=0,ancestor=0):
         sys.stdout.buffer.write(bytes(('encoding iso-8859-15\n'), 'utf-8')) #encoding for the following description ('iso-8859-15')
         sys.stdout.buffer.write(bytes(('data %d\n%s\n' % (len(revision["description"]), revision["description"])), 'iso-8859-15'))
         if ancestor:
-            sys.stdout.buffer.write(b'from :%d' % convert_revision_to_mark(ancestor)) #we're starting a development path so we need to start from it was originally branched from
+            sys.stdout.buffer.write(bytes(('from :%d\n' % convert_revision_to_mark(ancestor)), 'utf-8')) #we're starting a development path so we need to start from it was originally branched from
             ancestor = 0 #set to zero so it doesn't loop back in to here
         sys.stdout.buffer.write(b'deleteall\n')
         tree = os.walk('.')
@@ -140,13 +138,22 @@ def export_to_git(revisions,devpath=0,ancestor=0):
                     fullfile = filename
                 else:
                     fullfile = os.path.join(dir[0], filename)[2:]
-                if (fullfile.find('.pj') != -1):
+                # The *.pj files are used by MKS and should be skipped
+                if (fullfile.endswith('.pj')):
                     continue
                 if (fullfile[0:4] == ".git"):
                     continue
                 if (fullfile.find('mks_checkpoints_to_git') != -1):
                     continue
                 inline_data(fullfile)
+        # Check the contents of the revision label (may have been changed previously)
+        if(revision["label"] != "-"):
+            TmpStr = "%s__%s" % (revision["number"], revision["label"])
+        else: # Use MKS Revision number as tag only!
+            TmpStr = revision["number"]
+        # Create a "lightweight tag" with "reset command" for this commit
+        sys.stdout.buffer.write(bytes(('reset refs/tags/%s\n' % TmpStr), 'utf-8')) # MKS Checkpoint information as GIT tag
+        sys.stdout.buffer.write(bytes(('from :%d\n' % mark), 'utf-8'))             # specify commit for this tag by "mark"
 
 marks = []
 devpaths = retrieve_devpaths()
